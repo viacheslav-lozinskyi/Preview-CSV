ca1b9b0106f0b1e7424ec31509ecfbc95cdbccb5
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-05-04 19:36:15 +0300

    - Minor changes

--- application/preview-csv.vs/resource/package/VSPackage.cs
+++ application/preview-csv.vs/resource/package/VSPackage.cs
@@ -26,7 +26,7 @@ namespace resource.package
             public const string GUID = "B8FEC982-93E4-4BD9-885B-651BC87F0567";
             public const string HOST = "MetaOutput";
             public const string NAME = "Preview-CSV";
-            public const string VERSION = "1.0.15";
+            public const string VERSION = "1.1.0";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
